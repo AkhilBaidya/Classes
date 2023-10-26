e4d600946d4196a64b04f7686ed407bb3cc26fd4
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-26 10:24:27 -0700

    Beginnings of Video Game class (child of Media class)

---
+++ VideoGame.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include "DigitalMedia.h" //parent class
+
+using namespace std;
+
+class VideoGame : public DigitalMedia {
+
+ public:
+
+ protected:
+  
+};
