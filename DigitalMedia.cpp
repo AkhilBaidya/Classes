ec710278f1e240f61be9c2040824c217a8edaeac
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-26 10:08:22 -0700

    defined constructor function and the function that defines variables in the Media class

--- DigitalMedia.cpp
+++ DigitalMedia.cpp
@@ -7,11 +7,22 @@ using namespace std;
 //constructor function:
 DigitalMedia::DigitalMedia() {
 
+  //create space for the variables in the class:
+  title = new char[20];
+  year = new int;
+  
 }
 
 //constructor with setting variables:
-DigitalMedia::DigitalMedia(char title[20], int year) {
+DigitalMedia::DigitalMedia(char newTitle[20], int newYear) {
 
+  //create space for the variables:
+  title = new char[20];
+  year = new int;
+
+  //define them based on the inputs newTitle and newYear:
+  strcpy(title, newTitle);
+  year = newYear;
 }
 
 //getting the title:
