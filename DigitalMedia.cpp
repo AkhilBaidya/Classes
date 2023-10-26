973c9b5b018b6f35710130c079e21efd31482964
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-26 10:00:48 -0700

    Beginnings of .cpp file for Media class

---
+++ DigitalMedia.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include "DigitalMedia.h"
+
+using namespace std;
+
+//constructor function:
+DigitalMedia::DigitalMedia() {
+
+}
+
+//constructor with setting variables:
+DigitalMedia::DigitalMedia(char title[20], int year) {
+
+}
+
+//getting the title:
+DigitalMedia::getTitle() {
+
+}
+
+//getting the year:
+DigitalMedia::getYear() {
+
+}
