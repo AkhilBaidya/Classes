3e3e41041825f4fbea7c2b46e2772a5ee1851d0a
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-26 10:37:57 -0700

    begun defining functions of Video Game class

---
+++ VideoGame.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include "VideoGame.h"
+
+VideoGame::VideoGame() {
+
+}
+
+VideoGame::setPublisher(char newPublisher[50]) {
+  
+}
+
+VideoGame::getPublisher() {
+
+}
+
+VideoGame::setRating(int newRating) {
+
+}
+
+VideoGame::getRating() {
+
+}
