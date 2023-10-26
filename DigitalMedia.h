a9c0c9fd70c6a86b41c7c493c9b6dd820078dc6d
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-26 09:51:59 -0700

    Added public content in Parent Media Class

--- DigitalMedia.h
+++ DigitalMedia.h
@@ -7,5 +7,11 @@ using namespace std;
 
 class DigitalMedia {
 
+public:
 
+  DigitalMedia(); //constructor
+  DigitalMedia(char title[20], int year); //create a media with a specific title and year
+
+  char getTitle(); //function to get the title
+  char getYear(); //function to get the year
 };
