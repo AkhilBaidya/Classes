6b458fac379e630a5d79a3ba41c91738d10e9535
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-31 10:32:08 -0700

    added revisions with Struct (see commit comments for .cpp file) on this .h file

--- DigitalMedia.h
+++ DigitalMedia.h
@@ -5,14 +5,18 @@
 
 using namespace std;
 
-class DigitalMedia {
+class DigitalMedia { //idea to have title returned as a struct instead of an array from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function
 
+  struct Title {
+    char theTitle[20];
+  }
+  
 public:
 
   DigitalMedia(); //constructor
   DigitalMedia(char newTitle[20], int newYear); //create a media with a specific title and year
 
-  char getTitle(); //function to get the title
+  Title getTitle(); //function to get the title
   int getYear(); //function to get the year
 
   void setTitle(char newTitle[20]); //set just the title;
@@ -23,4 +27,5 @@ private:
   //the title and year specific to a piece of digital media:
   char title[20];
   int year;
+  Title Title;
 };
