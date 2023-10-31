f2a33c5a217577755208454146b28687f48c4bcd
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-31 10:49:37 -0700

    testing how classes work in main class

--- Main.cpp
+++ Main.cpp
@@ -4,8 +4,28 @@
 
 using namespace std;
 
+struct Title { //idea of encasing a character array in a Struct taken from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function, user CB Bailey
+
+  //since a character array cannot be returned from a function, an object that contains an array can be returned instead.
+
+  
+  char theTitle[20];
+};
+  
 int main() {
 
+  char input[20];
+  cin.get(input, 20);
+  
+  DigitalMedia* coolMedia = new DigitalMedia(input, 2023);
+
+  Title output;
+
+  output = coolMedia -> getTitle();
+  cout << output -> theTitle[0];
+  cout << coolMedia->getYear() << endl;
+  coolMedia -> setYear(1578);
+  cout << coolMedia->getYear() << endl;
 
   return 0;
 }
