a13f35116283bf9f066272956e0d65cd1aeb799b
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-02 10:11:12 -0700

    additionaly changed this file to make function types from Struct Title to char*

--- DigitalMedia.h
+++ DigitalMedia.h
@@ -2,30 +2,31 @@
 
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
 class DigitalMedia { //idea to have title returned as a struct instead of an array from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function
 
-  struct Title {
+  /*struct Title {
     char theTitle[20];
-  };
+    };*/
   
 public:
 
   DigitalMedia(); //constructor
-  DigitalMedia(char newTitle[20], int newYear); //create a media with a specific title and year
+  DigitalMedia(char* newTitle, int newYear); //create a media with a specific title and year
 
-  Title getTitle(); //function to get the title
+  char* getTitle(); //function to get the title
   int getYear(); //function to get the year
 
-  void setTitle(char newTitle[20]); //set just the title;
+  void setTitle(char* newTitle); //set just the title;
   void setYear(int newYear); //set just the year
 
 private:
 
   //the title and year specific to a piece of digital media:
-  char title[20];
+  char* title;
   int year;
-  Title Title;
+  //Title Title;
 };
