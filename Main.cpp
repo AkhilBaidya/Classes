8a8c9bb6032181de9cb6490d0f9d85948955ad25
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-02 10:08:13 -0700

    Tried using char* instead of Struct for function type, as seen in Mr. Galbraith's videos

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <cstring>
 #include "DigitalMedia.h"
+#include <vector>
 
 using namespace std;
 
-struct Title { //idea of encasing a character array in a Struct taken from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function, user CB Bailey
+/*struct Title { //idea of encasing a character array in a Struct taken from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function, user CB Bailey
 
   //since a character array cannot be returned from a function, an object that contains an array can be returned instead.
 
   
   char theTitle[20];
-};
+  };*/
   
 int main() {
 
-  char input[20];
+  char* input = new char[20];
   cin.get(input, 20);
   
   DigitalMedia* coolMedia = new DigitalMedia(input, 2023);
-
-  Title output;
+  
+  /*Title output;
 
   output = coolMedia -> getTitle();
   cout << output -> theTitle[0];
   cout << coolMedia->getYear() << endl;
   coolMedia -> setYear(1578);
   cout << coolMedia->getYear() << endl;
-
+  */
   return 0;
 }
