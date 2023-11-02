adecab398d40e522d4f2c50303cd9be357b7815c
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-02 10:12:37 -0700

    additionaly changed this file to make function types from Struct Title to char*

--- DigitalMedia.cpp
+++ DigitalMedia.cpp
@@ -1,48 +1,52 @@
 #include <iostream>
 #include <cstring>
 #include "DigitalMedia.h"
+#include <vector>
 
 using namespace std;
 
-struct Title { //idea to return a struct w/ an array instead of an array from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function
+/*struct Title { //idea to return a struct w/ an array instead of an array from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function
   char theTitle[20];
-};
+  };*/
 
 //constructor function:
 DigitalMedia::DigitalMedia() {
 
   //create space for the variables in the class:
   title = new char[20];
-  Title = new Title;
-  Title.theTitle = title; //create a Title struct to encase the title array
+  //Title = new Title;
+  //Title.theTitle = title; //create a Title struct to encase the title array
 
   year = new int;
   
 }
 
 //construct object of the class and set variables:
-DigitalMedia::DigitalMedia(char newTitle[20], int newYear) {
+DigitalMedia::DigitalMedia(vector<char> newTitle, int newYear) {
 
   //create space for the variables:
   title = new char[20];
-  Title = new Title;
-  Title.theTitle = title;
+  //Title = new Title;
+  //Title.theTitle = title;
   
   year = new int;
 
   //define them based on the inputs newTitle and newYear:
-  strcpy(title, newTitle);
+  strcpy(*title, *newTitle);
+
+  //title = newTitle;
   year = newYear;
 }
 
 //setting the title:
-void DigitalMedia::setTitle(char newTitle[20]) {
-  strcpy(title, newTitle); //set variable equal to input
+void DigitalMedia::setTitle(char* newTitle) {
+  strcpy(*title, *newTitle); //set variable equal to input
+  //title = newTitle;
 }
 
 //getting the title:
-Title DigitalMedia::getTitle() {
-  return Title;
+char* DigitalMedia::getTitle() {
+  return title;
 }
 
 //setting the year:
