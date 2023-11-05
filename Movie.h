ed1bd03c42d9f6b7f16c4e33a8ea56d1c4aa8a38
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 13:52:51 -0800

    beginnings of Movie class

---
+++ Movie.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include "DigitalMedia.h" //the parent class!
+
+//needs extra: director, duration, rating
+
+using namespace std;
+
+class Movie : public DigitalMedia {
+
+ public:
+
+
+
+ protected:
+
+  
+};
