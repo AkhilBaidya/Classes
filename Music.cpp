26307b3fd359e522edcae7bfcd64e84c5bd18c13
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 13:46:52 -0800

    function defs done for constructors

--- Music.cpp
+++ Music.cpp
@@ -3,10 +3,17 @@
 #include "Music.h"
 
 Music::Music() {
+  artist = new char[50];
+  duration = 0.00;
+  rating = 0.00;
 }
 
 Music::Music(char theTitle[20], int theYear, char theArtist[50], float theDuration, float theRating):DigitalMedia(theTitle, theYear) {
-
+  title = theTitle;
+  year = theYear;
+  artist = theArtist;
+  duration = theDuration;
+  rating = theRating;
 }
 
 void setArtist(char newArtist[50]) {
