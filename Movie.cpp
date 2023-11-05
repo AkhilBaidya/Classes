4e7a11c73fd5d2c11f878e55542aeb36e4dacb8f
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 14:04:17 -0800

    wrote constructor function defs'

--- Movie.cpp
+++ Movie.cpp
@@ -3,9 +3,17 @@
 #include "Movie.h"
 
 Movie::Movie() {
+  director = new char[50];
+  duration = 0.00;
+  rating = 0.00;
 }
 
 Movie::Movie(char theTitle[20], int theYear, char theDirector[50], float theDuration, float theRating):DigitalMedia(theTitle, theYear) {
+  title = theTitle;
+  year = theYear;
+  director = theDirector;
+  duration = theDuration;
+  rating = theRating;
 }
 
 void Movie::setDirector(char newDirector[50]) {
