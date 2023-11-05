03559f015f9ba6ca7d42e5bd59d3d85735b00049
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 13:29:53 -0800

    beginnings of Media class

---
+++ Music.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include "DigitalMedia.h" //the parent class
+
+//needs extra: artist, duration, rating
+
+using namespace std;
+
+class Music : public DigitalMedia {
+
+ public:
+
+
+ protected:
+
+
+};
