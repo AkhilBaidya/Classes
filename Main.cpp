2316de8ea9793ac860e1e7f9517af02e8e4e2615
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-04 19:56:35 -0700

    title and year can be got

--- Main.cpp
+++ Main.cpp
@@ -5,43 +5,33 @@
 
 using namespace std;
 
-/*struct Title { //idea of encasing a character array in a Struct taken from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function, user CB Bailey
-
-  //since a character array cannot be returned from a function, an object that contains an array can be returned instead.
-
+/*Source notes:
   
-  char theTitle[20];
+initial idea of encasing a character array in a Struct taken from https://stackoverflow.com/questions/4264304/how-to-return-an-array-from-a-function, user CB Bailey
+since a character array cannot be returned from a function, an object that containsan array can be returned instead.
+
+next idea to use char* for function types (that the function returns a char pointer to the array it takes in) is inspired by Mr. Galbraith's video on Classes in Canvas.
   };*/
   
 int main() {
 
-  //char* inputPntr = new char[20];
   char input[20]; 
   cin.get(input, 20);
-  char* inputPnt = input;
   
-  DigitalMedia* coolMedia = new DigitalMedia(input,1500);
+  DigitalMedia* coolMedia = new DigitalMedia(input,2024);
 
   char* output = coolMedia -> getTitle();
 
-  //strcpy(output, coolMedia -> getTitle());
-
-
-  /*for (int i; i< strlen(input); i++) {
-    cout << input[i];
-    }*/
-
-  cout << "in the for loop with " << strlen(output) << " length" << endl;
   for (int i = 0; i < strlen(output); i++) {
-    cout << "oh" << endl;
     cout << output[i];
     }
-  cout << "out of the for loop" << endl;
+
   
   cout << coolMedia->getYear() << endl;
   
   coolMedia -> setYear(1578);
 
   cout << coolMedia->getYear() << endl;
+
   return 0;
 }
