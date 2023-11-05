51a3b2d7f0b4883a38afaa86a02ddf9517893507
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-04 20:12:32 -0700

    defined constructor (sets variables)

--- VideoGame.cpp
+++ VideoGame.cpp
@@ -3,7 +3,15 @@
 #include "VideoGame.h"
 
 VideoGame::VideoGame() {
+  publisher = new char[50];
+  rating = 0.00; //default rating
+}
 
+VideoGame::VideoGame(char theTitle[20], int theYear, char thePublisher[50], float theRating) {
+  title = theTitle;
+  year = theYear;
+  publisher = thePublisher;
+  rating = theRating;
 }
 
 VideoGame::setPublisher(char newPublisher[50]) {
