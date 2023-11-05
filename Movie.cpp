1c2ead467eb823c04e823fec4efd1e13402b67e3
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 14:02:29 -0800

    writing function defs for class

--- Movie.cpp
+++ Movie.cpp
@@ -7,3 +7,22 @@ Movie::Movie() {
 
 Movie::Movie(char theTitle[20], int theYear, char theDirector[50], float theDuration, float theRating):DigitalMedia(theTitle, theYear) {
 }
+
+void Movie::setDirector(char newDirector[50]) {
+}
+
+char* Movie::getDirector(){
+}
+
+void Movie::setDuration(float newDuration){
+}
+
+float Movie::getDuration(){
+}
+
+void Movie::setRating(float newRating){
+}
+
+float Movie::getRating() {
+}
+
