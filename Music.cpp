0bf854e205ec09adb9a36fa7fd97da803992b2b7
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 13:42:05 -0800

    beginnings of function defs

---
+++ Music.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+#include "Music.h"
+
+Music::Music() {
+}
+
+Music::Music(char theTitle[20], int theYear, char theArtist[50], float theDuration, float theRating) {
+
+}
