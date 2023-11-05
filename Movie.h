125f6bd35f87e163900c9f6531220e59d2b5da90
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 13:54:56 -0800

    wrote fields and constructors for Movie class

--- Movie.h
+++ Movie.h
@@ -10,9 +10,15 @@ class Movie : public DigitalMedia {
 
  public:
 
+  Movie();
 
+  Movie(char theTitle[20], int theYear, char theDirector[50], float theDuration, float theRating);
+  
 
  protected:
 
+  char* director;
+  float duration;
+  float rating;
   
 };
