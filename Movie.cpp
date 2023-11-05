f10738713aee621bfeda22eeed2bbea1bd923a3a
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-05 14:05:57 -0800

    wrote function defs

--- Movie.cpp
+++ Movie.cpp
@@ -17,20 +17,25 @@ Movie::Movie(char theTitle[20], int theYear, char theDirector[50], float theDura
 }
 
 void Movie::setDirector(char newDirector[50]) {
+  director = newDirector;
 }
 
 char* Movie::getDirector(){
+  return director;
 }
 
 void Movie::setDuration(float newDuration){
+  duration = newDuration;
 }
 
 float Movie::getDuration(){
+  return duration;
 }
 
 void Movie::setRating(float newRating){
+  rating = newRating;
 }
 
 float Movie::getRating() {
+  return rating;
 }
-
