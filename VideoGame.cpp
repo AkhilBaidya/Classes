5444317dfd244f8debbcbacbec3c15db8b03b390
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-07 23:52:57 -0800

    found source of segmentation error with not including = new char[50]

--- VideoGame.cpp
+++ VideoGame.cpp
@@ -2,26 +2,28 @@
 #include <cstring>
 #include "VideoGame.h"
 
-VideoGame::VideoGame() {
+VideoGame::VideoGame():DigitalMedia() {
   publisher = new char[50];
   rating = 0.00; //default rating
   type = 1;
 }
 
 VideoGame::VideoGame(char theTitle[20], int theYear, char thePublisher[50], float theRating):DigitalMedia(theTitle, theYear) {
-  title = theTitle;
+  strcpy(title, theTitle);//make sure to use strcpy, otherwise I am setting my pointer equal to the other pointer (suggestion by my father)
   year = theYear;
-  publisher = thePublisher;
+  //publisher = thePublisher;
+  publisher = new char[50]; //cause of segmentation error -> forgot to include
+  strcpy(publisher, thePublisher);
   rating = theRating;
   type = 1;
 }
 
 VideoGame::~VideoGame() {
-  delete[] publisher;
+  delete[] publisher; //don't need [] since publisher is a pointer
 }
 
 void VideoGame::setPublisher(char newPublisher[50]) {
-  publisher = newPublisher;
+  strcpy(publisher,newPublisher);
 }
 
 char* VideoGame::getPublisher() {
