93144a43119ee8cc924257a9388af8dc66a05c01
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-07 23:53:46 -0800

    Working on using strcpy between char pointers not = which led to errors with accessing that field in the vector in Main.cpp

--- Music.cpp
+++ Music.cpp
@@ -6,14 +6,17 @@ Music::Music() {
   artist = new char[50];
   duration = 0.00;
   rating = 0.00;
+  type = 3;
 }
 
 Music::Music(char theTitle[20], int theYear, char theArtist[50], float theDuration, float theRating):DigitalMedia(theTitle, theYear) {
-  title = theTitle;
+  strcpy(title,theTitle);
   year = theYear;
-  artist = theArtist;
+  //artist = theArtist; //refer to VideoGame.cpp comments - setting pointer to another pointer was the main error I saw, where my vector of media, when iterated through, could not print titles
+  strcpy(artist, theArtist);
   duration = theDuration;
   rating = theRating;
+  type = 3;
 }
 
 void Music::setArtist(char newArtist[50]) {
