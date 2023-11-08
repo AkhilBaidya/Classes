bbbe675d1306b0d8b91b70301728917d8fd00e3e
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-08 10:03:45 -0800

    added functions for new field publisher

--- Music.cpp
+++ Music.cpp
@@ -5,18 +5,22 @@
 Music::Music() {
   artist = new char[50];
   duration = 0.00;
-  rating = 0.00;
+  publisher = new char[50]; //need publisher
   type = 3;
 }
 
-Music::Music(char theTitle[20], int theYear, char theArtist[50], float theDuration, float theRating):DigitalMedia(theTitle, theYear) {
+Music::Music(char theTitle[20], int theYear, char theArtist[50], char thePublisher[50], float theDuration):DigitalMedia(theTitle, theYear) {
   strcpy(title,theTitle);
   year = theYear;
   //artist = theArtist; //refer to VideoGame.cpp comments - setting pointer to another pointer was the main error I saw, where my vector of media, when iterated through, could not print titles
   artist = new char[50]; //added to avoid segmentation fault
   strcpy(artist, theArtist);
+
+  publisher = new char[50];
+  strcpy(publisher, thePublisher);
+
+  
   duration = theDuration;
-  rating = theRating;
   type = 3;
 }
 
@@ -26,6 +30,14 @@ void Music::setArtist(char newArtist[50]) {
 
 char* Music::getArtist() {
   return artist;
+}
+
+void Music::setPublisher(char newPublisher[50]) {
+  strcpy(publisher, newPublisher);
+}
+
+char* Music::getPublisher() {
+  return publisher;
 }
 
 void Music::setDuration(float newDuration) {
