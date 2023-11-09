d2fbcc7347836dde42dedfcd36e824f93738ec22
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-08 23:26:43 -0800

    Final commented version

--- Movie.h
+++ Movie.h
@@ -1,37 +1,51 @@
+//Classes Project: Movie (child) Class Header
+//by Akhil Baidya
+
+//Submission date: 11/8/2023
+
+/*About: The Movie Class Header states the unique fields and methods used in a Movie object, such as the fields of director, duration, and rating and their respective get and set methods.
+
+This class, as a child of the Digital Media Class, will inherit methods and fields from that parent class, such as the fields of title and its get and set methods.
+ */
+
 #include <iostream>
 #include <cstring>
 #include "DigitalMedia.h" //the parent class!
 
-//needs extra: director, duration, rating
-
 using namespace std;
 
 class Movie : public DigitalMedia {
+  //Movies have a Title, Year, Director, Duration, and Rating:
 
+
+  //PUBLIC CONTENT:
  public:
-  
+
+  //CONSTRUCTORS:
   Movie();
+  Movie(char theTitle[20], int theYear, char theDirector[50], float theDuration, float theRating); //construct class object with setting variables to inputs
 
-  Movie(char theTitle[20], int theYear, char theDirector[50], float theDuration, float theRating);
+  //DESTRUCTOR:
+  ~Movie();
 
-  ~Movie(); //destructor
-  
-  char* getDirector();
+  //GET METHODS:
+  char* getDirector(); //return the director name
 
-  float getDuration();
+  /*Source notes (as it appears in VideoGame.h): idea to return names with char* type methods from Mr. Galbraith's video on Canvas on Basic Classes and Vectors (refer to DigitalMedia.h for the link to the video): since c++ functions do not return arrays, returning a character pointer to an array can help with accessing that array representing a name */
 
-  float getRating();
+  float getDuration(); //return the duration (in minutes)
+  float getRating(); //return the movie rating 
 
-  void setDirector(char newDirector[50]);
+  //SET METHODS:
+  void setDirector(char newDirector[50]); //set director name to input
+  void setDuration(float newDuration); //set duration to input
+  void setRating(float newRating); //set rating to input
 
-  void setDuration(float newDuration);
 
-  void setRating(float newRating);
-  
+  //PROTECTED CONTENT: (fields unique to the Music class) 
  protected:
 
-  char* director;
-  float duration;
-  float rating;
-  
+  char* director; //director name
+  float duration; //duration of movie (in minutes)
+  float rating; //rating of movie
 };
